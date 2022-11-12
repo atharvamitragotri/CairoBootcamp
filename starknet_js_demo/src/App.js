3e383a6f70918479489824677f05d882da5a3d5f
atharva <atharva.mitragotri@gmail.com>
2022-11-13 04:23:02 +0530

    Homework 10 and 11

--- starknet_js_demo/src/App.js
+++ starknet_js_demo/src/App.js
@@ -4,10 +4,12 @@ import { connect } from "get-starknet"
 import { Contract } from "starknet"
 import { toBN } from "starknet/dist/utils/number"
 
-import contractAbi from "./contract_abi.json"
+// import contractAbi from "./contract_abi.json"
+import contractAbi from "./Storage_abi.json"
 
-const contractAddress = "0x0704ed6b41f5d9dfdc5037c627d53ee52aef0675ed47ba59b57b8152c0144a9e"
-
+// const contractAddress = "0x0704ed6b41f5d9dfdc5037c627d53ee52aef0675ed47ba59b57b8152c0144a9e"
+const contractAddress = "0x02701e09c6769717c9ca219426d9cec0d59e53f19a30b8635589440fb7626deb"
+// tx_hash = "0x07d6b63d1f0a48ee5aafa389fb7aa39ef9869f5b02f881248824b3b5c163e265"
 
 function App() {
   const [provider, setProvider] = useState('')
@@ -16,40 +18,40 @@ function App() {
   const [isConnected, setIsConnected] = useState(false)
 
 
-  const connectWallet = async() => {
-    try{
+  const connectWallet = async () => {
+    try {
       // connect the wallet
       const starknet = await connect()
       await starknet?.enable({ starknetVersion: "v4" })
       // set up the provider
       setProvider(starknet.account)
       // set wallet address
-     setAddress(starknet.selectedAddress)
+      setAddress(starknet.selectedAddress)
       // set connection flag
       setIsConnected(true)
-      
+
     }
-    catch(error){
+    catch (error) {
       alert(error.message)
     }
   }
 
-  const increaseBalanceFunction = async() => {
-    try{
+  const setBalanceFunction = async () => {
+    try {
       // create a contract object based on the provider, address and abi
       const contract = new Contract(contractAbi, contractAddress, provider)
-      
+
       // call the increase_balance function
-      await contract.increase_balance(13)
-      
+      await contract.set_balance(24)
+
     }
-    catch(error){
+    catch (error) {
       alert(error.message)
     }
   }
 
-  const getBalanceFunction = async() => {
-    try{
+  const getBalanceFunction = async () => {
+    try {
       // create a contract object based on the provider, address and abi
       const contract = new Contract(contractAbi, contractAddress, provider)
       // call the function
@@ -59,7 +61,7 @@ function App() {
       // display the result
       setRetrievedBalance(_decodedBalance)
     }
-    catch(error){
+    catch (error) {
       alert(error.message)
     }
   }
@@ -68,16 +70,16 @@ function App() {
       <header className="App-header">
         <main className="main">
           <h1 className="title">
-            Minimal Starknet JS DEMO
+            Starknet JS Homework
           </h1>
           {
             isConnected ?
-            <button className="connect">{address.slice(0, 5)}...{address.slice(60)}</button> :
-            <button className="connect" onClick={() => connectWallet()}>Connect wallet</button>
+              <button className="connect">{address.slice(0, 5)}...{address.slice(60)}</button> :
+              <button className="connect" onClick={() => connectWallet()}>Connect wallet</button>
           }
 
           <p className="description">
-            Using Starknet JS with a simple contract
+            Homework 11!
           </p>
 
           <div className="grid">
@@ -88,14 +90,14 @@ function App() {
               <div className="cardForm">
                 {/* <input type="text" className="input" placeholder="Enter Name" onChange={(e) => setName(e.target.value)} /> */}
 
-                <input type="submit" className="button" value="Add ETH  " onClick={() => increaseBalanceFunction()} />
+                <input type="submit" className="button" value="Set Balance  " onClick={() => setBalanceFunction()} />
               </div>
 
               <hr />
 
               {/* <p>Insert a wallet address, to retrieve its name.</p> */}
               <div className="cardForm">
-               
+
                 <input type="submit" className="button" value="Get Balance " onClick={() => getBalanceFunction()} />
               </div>
               <p>Balance: {retrievedBalance} ETH</p>
